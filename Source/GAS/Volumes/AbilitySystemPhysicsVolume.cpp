5c885fad0a794ecf07899099534c76bb185414a9
Neko <woerth.nicolas@gmail.com>
2023-09-23 11:13:38 +0200

    burn effect

---
+++ Source/GAS/Volumes/AbilitySystemPhysicsVolume.cpp
@@ -0,0 +1,123 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystemPhysicsVolume.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+AAbilitySystemPhysicsVolume::AAbilitySystemPhysicsVolume()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAbilitySystemPhysicsVolume::ActorEnteredVolume(AActor* Other)
+{
+	Super::ActorEnteredVolume(Other);
+
+	if (!HasAuthority()) return;
+
+	if (UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Other))
+	{
+		for (auto GA : PermanentAbilitiesToGive)
+		{
+			ASC->GiveAbility(FGameplayAbilitySpec(GA));
+		}
+
+		EnteredActorsInfoMap.Add(Other);
+
+		for (auto GA : OngoingAbilitiesToGive)
+		{
+			FGameplayAbilitySpecHandle AbilityHandle = ASC->GiveAbility(FGameplayAbilitySpec(GA));
+
+			EnteredActorsInfoMap[Other].AppliedAbilities.Add(AbilityHandle);
+		}
+
+		for (auto GE : OngoingEffectsToApply)
+		{
+			FGameplayEffectContextHandle EffectContextHandle = ASC->MakeEffectContext();
+
+			EffectContextHandle.AddInstigator(Other, Other);
+
+			FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(GE, 1, EffectContextHandle);
+			if (SpecHandle.IsValid())
+			{
+				FActiveGameplayEffectHandle ActiveGEHandle = ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+
+				if (ActiveGEHandle.WasSuccessfullyApplied())
+				{
+					EnteredActorsInfoMap[Other].AppliedEffects.Add(ActiveGEHandle);
+				}
+			}
+		}
+		for (auto EventTag : GameplayEventsToSendOnEnter)
+		{
+			FGameplayEventData EventPayload;
+			EventPayload.EventTag = EventTag;
+
+			UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Other, EventTag, EventPayload);
+		}
+	}
+}
+
+void AAbilitySystemPhysicsVolume::ActorLeavingVolume(AActor* Other)
+{
+	Super::ActorLeavingVolume(Other);
+
+	if (!HasAuthority()) return;
+
+	if (UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Other))
+	{
+		if (EnteredActorsInfoMap.Find(Other))
+		{
+			for (auto GEHandle : EnteredActorsInfoMap[Other].AppliedEffects)
+			{
+				ASC->RemoveActiveGameplayEffect(GEHandle);
+			}
+
+			for (auto GAHandle : EnteredActorsInfoMap[Other].AppliedAbilities)
+			{
+				ASC->ClearAbility(GAHandle);
+			}
+
+			for (auto GE : OnExitEffectsToApply)
+			{
+				FGameplayEffectContextHandle EffectContextHandle = ASC->MakeEffectContext();
+
+				EffectContextHandle.AddInstigator(Other, Other);
+
+				FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(GE, 1, EffectContextHandle);
+				if (SpecHandle.IsValid())
+				{
+					FActiveGameplayEffectHandle ActiveGEHandle = ASC->ApplyGameplayEffectSpecToSelf(
+						*SpecHandle.Data.Get());
+
+					if (ActiveGEHandle.WasSuccessfullyApplied())
+					{
+						EnteredActorsInfoMap[Other].AppliedEffects.Add(ActiveGEHandle);
+					}
+				}
+			}
+			
+			EnteredActorsInfoMap.Remove(Other);
+
+			for (auto EventTag : GameplayEventsToSendOnExit)
+			{
+				FGameplayEventData EventPayload;
+				EventPayload.EventTag = EventTag;
+
+				UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Other, EventTag, EventPayload);
+			}
+		}
+	}
+}
+
+void AAbilitySystemPhysicsVolume::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (bDrawDebug)
+	{
+		DrawDebugBox(GetWorld(), GetActorLocation(), GetBounds().BoxExtent, FColor::Red, false, 0, 0, 5);
+	}
+}
---
+++ Source/GAS/Volumes/AbilitySystemPhysicsVolume.h
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "GameFramework/PhysicsVolume.h"
+#include "AbilitySystemPhysicsVolume.generated.h"
+
+
+struct FGameplayEffectSpecHandle;
+struct FGameplayAbilitySpecHandle;
+class UGameplayAbility;
+struct FGameplayTag;
+class UGameplayEffect;
+
+USTRUCT(BlueprintType)
+struct FAbilityVolumeEnteredActorInfo
+{
+	GENERATED_BODY()
+
+	TArray<FGameplayAbilitySpecHandle> AppliedAbilities;
+
+	TArray<FActiveGameplayEffectHandle> AppliedEffects;
+};
+
+UCLASS()
+class GAS_API AAbilitySystemPhysicsVolume : public APhysicsVolume
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<UGameplayEffect>> OngoingEffectsToApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<UGameplayEffect>> OnExitEffectsToApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bDrawDebug = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FGameplayTag> GameplayEventsToSendOnEnter;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FGameplayTag> GameplayEventsToSendOnExit;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<UGameplayAbility>> OngoingAbilitiesToGive;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<UGameplayAbility>> PermanentAbilitiesToGive;
+
+	UPROPERTY()
+	TMap<AActor*, FAbilityVolumeEnteredActorInfo> EnteredActorsInfoMap;
+
+public:
+	AAbilitySystemPhysicsVolume();
+
+	virtual void ActorEnteredVolume(AActor* Other) override;
+
+	virtual void ActorLeavingVolume(AActor* Other) override;
+
+	virtual void Tick(float DeltaSeconds) override;
+};
